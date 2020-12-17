583753ea77e1ba026aa556053ac7f7ed1f14c0d2
Barbara Silva <bsm.97@outlook.com>
2020-12-16 23:45:07 -0300

    Se sube primera actividad javascript

---
+++ actividad javascrip U1/main.js
@@ -0,0 +1,9 @@
+function suma(){
+    var primera = 40
+    var segunda = 16
+    var sumatotal= numero1 + numero2;
+    document.getElementById("content").innerHTML = sumatotal;
+
+    console.log("El resultado de la suma de dos variables es:" + sumatotal);
+   
+}
